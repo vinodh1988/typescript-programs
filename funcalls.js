9f4fd42f7408d283c6b00b16abf2f395cecce4f9
vinodhleo <vinodhleo@outlook.com>
2022-03-29 16:14:29 +0530

    typescript

--- funcalls.js
+++ funcalls.js
@@ -1,6 +1,14 @@
 function add(a, b) {
     return a + b;
 }
+function sum(a) {
+    var sum = 0;
+    for (var x in a)
+        sum += a[x];
+    return sum;
+}
 add(34, 53);
-add(34);
+console.log(sum([43, 534, 53]));
+console.log(sum([34, "raj"]));
+//add(34)
 //add("Raj","Ravi")
--- funcalls.ts
+++ funcalls.ts
@@ -2,6 +2,15 @@ function add(a:number,b:number):number{
     return a+b
 }
 
+function sum(a:number[]):number
+{
+    let sum:number=0;
+    for(let x in a)
+      sum+=a[x]
+    return sum
+}
 add(34,53)
+console.log(sum([43,534,53]))
+//console.log(sum([34,"raj"]))
 //add(34)
 //add("Raj","Ravi")
