04b6db51f28ee40847316a7d8baab5b358d26dce
vinodhleo <vinodhleo@outlook.com>
2022-03-29 16:23:51 +0530

    object-type

---
+++ funcalls2.js
@@ -0,0 +1,6 @@
+function start(access) {
+    console.log(access[0]);
+    //  console.log(access.name)
+}
+//start({name:"Ravi",city:"Mumbai"})
+start([34, 35, 35]);
---
+++ funcalls2.ts
@@ -0,0 +1,7 @@
+function start(access:object):void{
+    console.log(access[0])
+  //  console.log(access.name)
+}
+
+//start({name:"Ravi",city:"Mumbai"})
+start([34,35,35])
