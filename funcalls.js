2036976ed0102cfe01d1281899cea5d56cd17062
vinodhleo <vinodhleo@outlook.com>
2022-03-29 16:06:35 +0530

    function-commit

---
+++ funcalls.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+add(34, 53);
+add(34);
+//add("Raj","Ravi")
---
+++ funcalls.ts
@@ -0,0 +1,7 @@
+function add(a:number,b:number):number{
+    return a+b
+}
+
+add(34,53)
+//add(34)
+//add("Raj","Ravi")
